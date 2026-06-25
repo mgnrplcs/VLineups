6cd2de4a579b6b33526bcfd3c572c3bdf94f4c86
mgnrplcs <thedexenzoredd@inbox.ru>
2026-06-25 22:31:27 +0300

    feat: base project setup

---
+++ frontend/src/components/ui/VButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <button class="bg-[#ff4655] text-white p-2 font-bold uppercase text-sm tracking-wider hover:bg-white hover:text-black transition-colors duration-200">
+        <slot>Нажми меня</slot>
+    </button>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup> 
+
+import VButton from '@/components/ui/VButton.vue';
+</script>
+
+<template>
+    <div>
+        <h1>
+            Welcome to VLineups!
+        </h1>
+        <p>123</p>
+
+        <VButton>test</VButton>
+    </div>
+</template>
