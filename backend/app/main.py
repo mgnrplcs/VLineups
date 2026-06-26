deba0e3e17fc96116cfe8bbed61e39a85e2fc13a
icanstillhearyou <smilecom333@yandex.ru>
2026-06-26 13:59:24 +0300

    chore: setup backend configuration and CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from config import settings
 
 app = FastAPI(
-    title="Vlineups"
+    title="VLineups", debug=settings.DEBUG, docs_url="/api/docs", redoc_url="/api/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=settings.CORS_METHODS,
+    allow_headers=settings.CORS_HEADERS,
 )
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to VLineups", "docs": "/api/docs"}
